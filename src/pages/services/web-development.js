57371108486c096595548ebd320aaffb41446fec
Aniruddha Bhandare <anirudhabhandare4303@gmail.com>
2025-08-01 01:39:33 +0530

    new

--- src/pages/services/web-development.js
+++ src/pages/services/web-development.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function WebDevHome() {
   return (
     <div style={{ padding: '40px' }}>
@@ -5,7 +6,7 @@ export default function WebDevHome() {
       <ul>
        <Link href="/services/web-development/mern/">MERN</Link>
         <Link href="/services/web-development/mean/">MEAN</Link>
-        <Link href="/services/web-development/dotnet/">.NET</Link>
+        <Link href="/services/web-development/dotnet">.NET</Link>
       </ul>
     </div>
   );
